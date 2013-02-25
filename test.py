93df978876679e0990d0f05daf2af4e2522ecb45
natto <natto@natto-tp.(none)>
2013-02-24 19:33:07 -0500

    wrap processor into function

--- test.py
+++ test.py
@@ -16,82 +16,87 @@ def cleanstring(s):
 
 tagged = '''<span style="background-color: yellow;">%s</span>'''
 
-# preprocess html since we don't care about whitespace
-html = cleanstring(open("/tmp/epubtest/kindle_split_011.html").read().decode('utf-8'))
-
-
-lsmatchidx = []
-
-nextbeg, stop = 0, len(html)
-lastend = 0
-while nextbeg < stop:
-    match = p_tag.search(html, nextbeg)
-    if match is None: break
-    lsmatchidx.append(Rec(nextbeg, match.start(), html[nextbeg:match.start()]))
-    nextbeg = match.end()
-
-fulltext = ("".join([cleanstring(rec.txt) for rec in lsmatchidx])).replace('\n', ' ')
-
-lsinsertion = []
-
 note = yaml.load(open("note.yml"))
-lsmatcher = [cleanstring(entry['highlight'].strip()) for entry in note]
 
-for matcher in lsmatcher:
-    idxmatchbeg = fulltext.find(matcher.replace('\n', ' '))
-    if idxmatchbeg > -1:
-        # find the real offset
-        contentlen = 0
+html = open("/tmp/epubtest/kindle_split_011.html").read().decode('utf-8')
 
-        lenremain = None
-        for irec, rec in enumerate(lsmatchidx):
+def process_html(note, html_orig):
+    html = cleanstring(html_orig)
 
-            if lenremain is not None:
-                if lenremain > 0:
+    # preprocess html since we don't care about whitespace
 
-                    grabtxt = rec.txt[:lenremain]
-                    # print "SECOND GO: >>>>>>>>>> ", grabtxt
-                    # print "LEN", lenremain
-                    # print "ORIG:", rec.txt
-                    rec.txt = tagged % (html[rec.beg:rec.beg+len(grabtxt)])
-                    # print rec.txt
-                    lenremain -= len(grabtxt)
-                if lenremain is 0:
-                    break
+    lsmatchidx = []
 
-            lastcontentlen = contentlen
-            contentlen += rec.end - rec.beg
-            # print contentlen, idxmatchbeg
-            if lenremain is None and idxmatchbeg <= contentlen:
-                # offset localized to the current
-                # rec object
-                reclen = len(rec.txt)
-                recoffsetbeg = idxmatchbeg - lastcontentlen
-                recoffsetend = recoffsetbeg + min(reclen, len(matcher))
-                
-                modbeg = rec.beg + recoffsetbeg
-                # modend = modbeg + len(rec.txt[]) len(matcher)
-                # print "the rec txt >>>>>>>>>>>>>>>", rec.txt
-                grabtxt = rec.txt[recoffsetbeg:recoffsetend]
-                rec.txt = rec.txt[:recoffsetbeg] + (tagged % grabtxt) + rec.txt[recoffsetend:]
-                # print irec, (tagged % (html[modbeg:modend]))
-                lsinsertion.append((rec.beg + recoffsetbeg, rec.beg + recoffsetbeg + len(matcher)))
+    nextbeg, stop = 0, len(html)
+    lastend = 0
+    while nextbeg < stop:
+        match = p_tag.search(html, nextbeg)
+        if match is None: break
+        lsmatchidx.append(Rec(nextbeg, match.start(), html[nextbeg:match.start()]))
+        nextbeg = match.end()
 
-                # hit a tag boundary, slurp the rest in next iter
-                if len(matcher) > reclen:
-                    lenremain = len(matcher) - len(grabtxt)
-                    continue
-                else:
-                    break
+    fulltext = ("".join([cleanstring(rec.txt) for rec in lsmatchidx])).replace('\n', ' ')
 
-lsout = []
-lastend = 0
-for rec in lsmatchidx:
-    lsout.append(html[lastend:rec.beg])
-    lsout.append(rec.txt)
-    lastend = rec.end
-lsout.append(html[lastend:])
+    lsinsertion = []
+
+    lsmatcher = [cleanstring(entry['highlight'].strip()) for entry in note]
+
+    for matcher in lsmatcher:
+        idxmatchbeg = fulltext.find(matcher.replace('\n', ' '))
+        if idxmatchbeg > -1:
+            # find the real offset
+            contentlen = 0
+
+            lenremain = None
+            for irec, rec in enumerate(lsmatchidx):
+
+                if lenremain is not None:
+                    if lenremain > 0:
+
+                        grabtxt = rec.txt[:lenremain]
+                        # print "SECOND GO: >>>>>>>>>> ", grabtxt
+                        # print "LEN", lenremain
+                        # print "ORIG:", rec.txt
+                        rec.txt = tagged % (html[rec.beg:rec.beg+len(grabtxt)])
+                        # print rec.txt
+                        lenremain -= len(grabtxt)
+                    if lenremain is 0:
+                        break
+
+                lastcontentlen = contentlen
+                contentlen += rec.end - rec.beg
+                # print contentlen, idxmatchbeg
+                if lenremain is None and idxmatchbeg <= contentlen:
+                    # offset localized to the current
+                    # rec object
+                    reclen = len(rec.txt)
+                    recoffsetbeg = idxmatchbeg - lastcontentlen
+                    recoffsetend = recoffsetbeg + min(reclen, len(matcher))
+
+                    modbeg = rec.beg + recoffsetbeg
+                    # modend = modbeg + len(rec.txt[]) len(matcher)
+                    # print "the rec txt >>>>>>>>>>>>>>>", rec.txt
+                    grabtxt = rec.txt[recoffsetbeg:recoffsetend]
+                    rec.txt = rec.txt[:recoffsetbeg] + (tagged % grabtxt) + rec.txt[recoffsetend:]
+                    # print irec, (tagged % (html[modbeg:modend]))
+                    lsinsertion.append((rec.beg + recoffsetbeg, rec.beg + recoffsetbeg + len(matcher)))
+
+                    # hit a tag boundary, slurp the rest in next iter
+                    if len(matcher) > reclen:
+                        lenremain = len(matcher) - len(grabtxt)
+                        continue
+                    else:
+                        break
+
+    lsout = []
+    lastend = 0
+    for rec in lsmatchidx:
+        lsout.append(html[lastend:rec.beg])
+        lsout.append(rec.txt)
+        lastend = rec.end
+    lsout.append(html[lastend:])
+    return "\n".join(lsout)
 
 with open("/tmp/testout.html", "w") as ofile:
-    ofile.write(("\n".join(lsout)).encode('utf-8'))
+    ofile.write(process_html(note, html).encode('utf-8'))
 
