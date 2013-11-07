e544e8a062c0895371cb21f68b0851da8477f643
natto <natto@natto-tp.none>
2013-11-07 18:23:31 -0500

    add experimental code for more intelligent word anchoring

---
+++ anchortext.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+
+
+import json
+from tfidf import *
+
+
+PRE_MULTIPLIER = 10
+POST_MULTIPLIER = 10
+
+class Anchor:
+
+    def __init__(self):
+        self.token = None
+        self.approximate_offset = None
+        self.support_anchor_list = []
+
+    def __repr__(self):
+        rtn = "[%s] ~ %s" % (self.token, self.approximate_offset)
+        if self.support_anchor_list:
+            rtn += "\n    supported by:\n        " + "\n        ".join(map(str, self.support_anchor_list))
+        return rtn
+
+    def to_json(self):
+        return json.dumps(dict(
+            token = self.token,
+            aoffset = self.approximate_offset,
+            support = [anc.to_json() for anc in self.support_anchor_list],
+            ))
+
+
+def apply_insertion(base_text, insertion_list):
+    """
+    `insertion_list`: [(index:int, text:string)]: inserts text at index
+    """
+    insertion_list.sort()
+    for index, text in sorted(insertion_list, reverse = True):
+        base_text = base_text[:index] + text + base_text[index:]
+    return base_text
+
+def apply_anchor_range(anchor0, anchor1, base_text):
+    return apply_anchor(anchor0, base_text), apply_anchor(anchor1, base_text)+len(anchor1.token)
+
+def apply_anchor(anc, base_text):
+    """
+    returns exact begin and end offset
+    based on `anchor`
+    """
+
+    idx_start = max(0,              anc.approximate_offset - len(anc.token) * PRE_MULTIPLIER)
+    idx_end   = min(len(base_text), anc.approximate_offset + len(anc.token) * POST_MULTIPLIER) - len(anc.token)
+
+    match_list = []
+    idx_current = idx_start
+    while idx_current < idx_end:
+        idx_match = base_text[idx_current:].find(anc.token)
+        if idx_match == -1:
+            break
+        match_list.append(idx_current+idx_match)
+        idx_current = idx_current + idx_match + len(anc.token)
+
+    print "LENGHT OF MATCH LIST", len(match_list)
+    print match_list
+    if len(match_list) is 1:
+        return match_list[0]
+    elif len(match_list) is 0:
+        raise Exception("inexistent")
+    else:
+        # find the closest to approximate_offset and return that
+        rank_list = [(abs(idx - anc.approximate_offset), idx) for idx in match_list]
+        rank_list.sort()
+        return rank_list[0][1]
+
+def make_anchor(desired_token, approximate_offset, current_document_index, base_document_list):
+
+    idx_start = approximate_offset - len(desired_token) * PRE_MULTIPLIER
+    idx_end   = approximate_offset + len(desired_token) * (POST_MULTIPLIER+1)
+
+    anc = Anchor()
+    anc.token = desired_token
+    anc.approximate_offset = approximate_offset
+
+    tfidfer = TFIDF(base_document_list)
+
+    for score, firstidx, token in tfidfer.bestn(current_document_index, N = 20):
+        if len(anc.support_anchor_list) > 5: break
+        if token == desired_token: continue
+        support_anchor = Anchor()
+        support_anchor.token = token
+        support_anchor.approximate_offset = firstidx
+        anc.support_anchor_list.append(support_anchor)
+
+    return anc
+
+def make_anchor_range(desired_text, approximate_offset, current_document_index, base_document_list):
+    spl = tokenize(desired_text)
+    token0 = spl[0]
+    token1 = spl[-1]
+    offset0 = approximate_offset
+    offset1 = approximate_offset + len(desired_text) - len(token1)
+
+    anchor0 = make_anchor(token0, offset0, current_document_index, base_document_list)
+    anchor1 = make_anchor(token1, offset1, current_document_index, base_document_list)
+
+    return anchor0, anchor1
+
+
+if __name__ == "__main__":
+
+
+    try:
+        from termcolor import colored
+        USE_TERMCOLOR = True
+    except Exception, e:
+        USE_TERMCOLOR = False
+
+
+
+    document_text_list = [
+            """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. 
+            """,
+            """
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. 
+            """,
+            """
+Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. this particular one is not the target is this the real no it is not but this target no also a fake this is the is it real not really no so actually this is the real target text but there is other text the rest is just there to confuse you but this is also real is this real the target is real this is but is that real this real there Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. 
+
+            """,
+            """
+Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. 
+            """,
+            """
+Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. 
+            """,
+            ]
+    highlighted_text = "this is the real target text"
+
+    if True:
+        offset_begin = 19
+        testtxt = document_text_list[2]
+        anc = make_anchor(highlighted_text, offset_begin, 2, document_text_list)
+        ra0, ra1 = make_anchor_range(highlighted_text, offset_begin, 2, document_text_list)
+        idx0, idx1 = apply_anchor_range(ra0, ra1, testtxt)
+        print testtxt[:idx0] + colored(testtxt[idx0:idx1], "yellow", attrs=["bold"]) + testtxt[idx1:]
+        idx = apply_anchor(anc, testtxt)
+
+        if USE_TERMCOLOR:
+            print testtxt[:idx] + colored(testtxt[idx:idx+len(anc.token)], "yellow", attrs=["bold"]) + testtxt[idx+len(anc.token):]
+        else:
+            print apply_insertion(testtxt, [(idx, "*"), (idx+len(anc.token), "*")])
+
+    if False:
+        testtxt = """
+        we are the best the best, the the the best!
+        """.strip()
+        anc = Anchor()
+        anc.token = "the"
+        anc.approximate_offset = 18
+        idx = apply_anchor(anc, testtxt)
+
+        if USE_TERMCOLOR:
+            print testtxt[:idx] + colored(testtxt[idx:idx+len(anc.token)], "yellow", attrs=["bold"]) + testtxt[idx+len(anc.token):]
+        else:
+            print apply_insertion(testtxt, [(idx, "*"), (idx+len(anc.token), "*")])
+
+
+    ## test word scoring method
+    if False:
+        for word in """
+        test
+        easy
+        think
+        quartz
+        abc123fourvi
+        queen
+        zenith
+        the
+        """.strip().split():
+            print word, get_word_score(word)
---
+++ tfidf.py
@@ -0,0 +1,164 @@
+import os, sys
+import yaml
+import epub
+import re
+from lxml import etree
+import math
+import random
+
+from collections import Counter
+
+
+# TODO
+# if document_list is small should use some heuristic to measure word
+# complexity, and prefer to anchor on rare words
+
+## here's the simple rare word scoring heuristic
+## this chart from http://www.math.cornell.edu/~mec/2003-2004/cryptography/subs/frequencies.html
+## retrieved on Thu Nov  7 17:30:49 EST 2013
+dletterscore = dict([(ch, 14-float(score)) for ch, score in map(str.split, """
+E   12.02
+T   9.10
+A   8.12
+O   7.68
+I   7.31
+N   6.95
+S   6.28
+R   6.02
+H   5.92
+D   4.32
+L   3.98
+U   2.88
+C   2.71
+M   2.61
+F   2.30
+Y   2.11
+W   2.09
+G   2.03
+P   1.82
+B   1.49
+V   1.11
+K   0.69
+X   0.17
+Q   0.11
+J   0.10
+Z   0.07
+""".strip().split("\n"))])
+def get_word_score(word):
+    modword = re.sub(r'[^a-zA-Z]', '', word)
+    return sum(map(dletterscore.get, modword.upper())) / len(modword) * math.log(len(word))
+
+def tokenize(s):
+    return re.split(r"[\W\s]", s)
+
+def tf(term, document):
+    """term frequency
+
+    - `term`: the search token
+    - `document`: a string that is just a bunch of text that should contain some number of instances of `term`
+    """
+    return tokenize(document.lower()).count(term.lower())
+
+def idf(term, document_list):
+    """inverse document frequency
+    
+    - `document_list`: list of string
+    """
+    n_doc_total = float(len(document_list))
+    n_doc_wterm = len(filter(lambda doc: 1 if tf(term, doc) > 0 else 0, document_list))
+    return math.log(n_doc_total / n_doc_wterm)
+
+def tfidf(term, idx_document, document_list):
+    return tf(term, document_list[idx_document]) * idf(term, document_list)
+
+class TFIDF:
+
+    dstopword = set("""
+    I a about an are as at be by com for from how in is it of on or that the this to was what when where who will with the
+    """.strip().split())
+
+    _dmemo = {}
+    def __init__(self, document_list):
+        self.document_list = document_list
+        self.ndoc = len(document_list)
+        for i, document in enumerate(document_list):
+            self._dmemo[i] = Counter(tokenize(document.lower()))
+
+    def tfidf(self, term, idx_document):
+        term = term.lower()
+        n_doc_wterm = len(filter(lambda doc_id: self._dmemo[doc_id].get(term) or 0, range(self.ndoc)))
+        return self._dmemo[idx_document][term] * math.log(float(self.ndoc) / n_doc_wterm)
+
+    def bestn(self, docidx, N = 6):
+        """
+
+        returns: a tuple of (tfidf score, index of *first* occurence, token in question)
+        
+        """
+        dcount = {}
+        # save index of occurence, we want to give precedence to earlier
+        # indexes (for human readability if one somehow wants to scan through
+        # the text for the matching anchor keyword), so save the first matching
+        # token's index
+        for i, token in enumerate(tokenize(self.document_list[docidx])):
+            if token not in dcount:
+                dcount[token] = []
+            dcount[token].append(i)
+
+        used = set()
+        res = []
+        for token, idxlist in dcount.iteritems():
+            if token in self.dstopword: continue
+            ltoken = token.lower()
+            if ltoken in used: continue
+            used.add(ltoken)
+            res.append((self.tfidf(token, docidx), idxlist[0], token))
+        res.sort()
+        return res[-N:]
+
+if __name__ == "__main__":
+
+    book = epub.open_epub("test.epub")
+
+    mydc = dict((key, book.read_item(item)) for key, item in filter(lambda (k, i): k.startswith("html"), book.opf.manifest.items()))
+
+    ls_text = []
+    for key in sorted(mydc.keys(), lambda a, b: int(a[4:]) > int(b[4:]) and 1 or -1):
+        xml = mydc[key]
+        tree = etree.fromstring(xml)
+
+        ## simple xpath won't work due to namespace prefixing
+        ## either use this notation
+        ## tree.xpath("//xhtml:style", namespaces={'xhtml': tree.nsmap[None]})
+        ## here, the None ns is actually the 'xhtml' ns
+        ## though i think you can redefine it to whatever you want
+        ## as opposed to 'xhtml' in the query
+        ## or this
+        for node in tree.xpath('''//*[local-name() = "style"]'''):
+            node.getparent().remove(node)
+        text = etree.tostring(tree, encoding = "utf8", method = "text")
+        ls_text.append(text)
+
+    tfidfer = TFIDF(ls_text)
+    
+    docnum = random.randint(1, len(ls_text))
+    docnum = 22
+    docidx = docnum - 1
+    if False:
+        res = []
+        alltoken = set(tokenize(" ".join(ls_text[docidx:docidx+1])))
+        print "DOCNUM: ", docnum
+        for i, token in enumerate(alltoken):
+            sys.stdout.write("%04d / %04d\r" % (i, len(alltoken)))
+            sys.stdout.flush()
+            res.append((tfidfer.tfidf(token, docidx), token))
+        print
+        res.sort()
+    else:
+
+        res = tfidfer.bestn(docidx)
+
+    for score, firstidx, token in res:
+        print token, "\t", score, firstidx
+
+
