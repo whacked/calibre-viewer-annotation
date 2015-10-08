f7cf07069af959c839531ec0f6932297da8d4380
natto <natto@natto-tpy.none>
2015-10-08 10:27:20 -0400

    add a little bit more intelligence to highlights downloader

--- get_kindle_highlights.rb
+++ get_kindle_highlights.rb
@@ -1,21 +1,54 @@
+###
+# script to download all available notes from amazon kindle into
+# yml files for further processing.
+# USAGE:
+# ruby get_kindle_highlights.rb [output-directory-path]
+
 require 'yaml'
+require 'io/console'
 require 'kindle_highlights'
 
 def setup
-    KindleHighlights::Client.new('email', 'password')
+  puts 'amazon account email: '
+  email = STDIN.gets.strip
+  puts 'amazon account password:'
+  passwd = STDIN.noecho(&:gets).strip
+  KindleHighlights::Client.new(email, passwd)
 end
 
-def save(k, id)
+def make_output_filename(book_name)
+  return "#{book_name}.yml"
+end
+
+def save(basedir, k, id)
   puts k.books[id]
   d = {
     'title' => k.books[id],
     'highlight_list' => k.highlights_for(id),
   }
-  ofile = "/tmp/#{k.books[id]}.yml"
+  ofile = File.join(basedir, make_output_filename(k.books[id]))
   File.open(ofile, 'w') {|f| f.write(d.to_yaml)}
   "ok: #{ofile}"
 end
 
-## demo usage:
-# k = setup
-# k.books.keys.each do |bkid| save(k, bkid) end
+if __FILE__ == $0
+  # if in irb,
+  # load 'get_kindle_highlights.rb'
+  
+  output_dir = ARGV[0] || 'kindle-highlights'
+  if not File.exists? output_dir then
+    puts "creating output directory: #{output_dir}"
+    Dir.mkdir output_dir
+  end
+  k = setup
+  k.books.keys.each do |bkid|
+    candidate_output = File.join(output_dir, make_output_filename(k.books[bkid]))
+    if File.exists? candidate_output
+      puts "*** already saved:\n    #{k.books[bkid]}"
+      puts "delete \"#{candidate_output}\" if you want to re-download it."
+      next
+    end
+    puts "saving #{bkid}"
+    save(output_dir, k, bkid)
+  end
+end
