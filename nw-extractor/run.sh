c30d7956c886ec0eb0cd26abb696176478782d04
natto <natto@natto-tpy.none>
2016-02-06 09:30:36 -0500

    upd to general `nw` binary call

--- nw-extractor/run.sh
+++ nw-extractor/run.sh
@@ -10,5 +10,6 @@ else
     export LD_LIBRARY_PATH=$([ -n "$LD_LIBRARY_PATH" ] && echo "$HERE:$HERE/lib:$LD_LIBRARY_PATH" || echo "$HERE:$HERE/lib")
     echo $LD_LIBRARY_PATH
 
-    exec -a "$0" "/opt/node-webkit-v0.11.1-linux-x64/nw" . "$@"
+    #exec -a "$0" "/opt/node-webkit-v0.11.1-linux-x64/nw" . "$@"
+    nw $@
 fi
