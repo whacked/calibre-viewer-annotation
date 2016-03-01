b421b0ceab31ef14d049b759b88637239c532a73
natto <natto@natto-tpy.none>
2016-03-01 17:55:13 -0500

    upd downloader with cache + better note matchup

--- get_kindle_highlights.rb
+++ get_kindle_highlights.rb
@@ -86,19 +86,64 @@ def setup
   return kindle
 end
 
-def make_output_filename(book_name)
-  return "#{book_name}.yml"
+
+# caching
+if not defined? $cached_mech_get       then $cached_mech_get = {} end
+if not defined? $cached_highlights_for then $cached_highlights_for  = {} end
+
+
+def load_page(kindle, endpoint)
+  result = $cached_mech_get[endpoint]
+  if result.nil?
+    puts "loading endpoint: #{endpoint}"
+    mech = kindle.instance_variable_get(:@mechanize_agent)
+    result = $cached_mech_get[endpoint] = mech.get(endpoint)
+  else
+    puts "reading from cache: #{endpoint}"
+  end
+  result
 end
 
-def save(basedir, k, id)
-  puts k.books[id]
-  d = {
-    'title' => k.books[id],
-    'highlight_list' => k.highlights_for(id),
+def parse_highlights_page(page)
+  # assumes each endLocation can only have at most 1 highlight and 1 note
+  # attached to it.
+  # `out` is the return structure
+  if page.is_a? Nokogiri::HTML::Document
+    ndoc = page
+  elsif page.is_a? String
+    ndoc = Nokogiri::HTML page
+  elsif page.is_a? Mechanize::Page
+    ndoc = Nokogiri::HTML page.body
+  end
+  top_match = ndoc.css('.bookMain.yourHighlightsHeader')[0]
+
+  out = {
+    title: top_match.css('.title').text,
+    asin: top_match.css('.title a').attr('href').value.split('/')[-1],
+    lastHighlighted: Date.parse(top_match.css('.lastHighlighted').text),
+    highlightMapping: {}, # endLocation => highlight text
+    noteMapping: {}, # endLocation => note text
   }
-  ofile = File.join(basedir, make_output_filename(k.books[id]))
-  File.open(ofile, 'w') {|f| f.write(d.to_yaml)}
-  "ok: #{ofile}"
+  top_match.css('.yourHighlightsStats').css('.boldText').each do |stat_item|
+    # boldText highlightCountXYZXYZ
+    # boldText noteCountXYZXYZ
+    kname = stat_item.attr('class').sub(/.* (.+Count).+/, '\1').to_sym
+    out[kname] = stat_item.text
+  end
+
+  ndoc.css('.highlightRow.yourHighlight').each do |entry|
+    # need to match the endLocation => highlight mapping to be a hit
+    highlight = entry.css('.highlight').text
+    endLocation = entry.css('.hidden.end_location').text.to_i
+
+    note_text = entry.css('.noteContent').text
+    if note_text.length > 0 then
+      out[:noteMapping][endLocation] = note_text
+    end
+
+    out[:highlightMapping][endLocation] = highlight
+  end
+  return out
 end
 
 def get_combined_annotation_list(kindle, asin)
@@ -109,55 +154,88 @@ def get_combined_annotation_list(kindle, asin)
   # both, and combine them.
 
   # first retrieve the highlight list.
-  highlight_list = kindle.highlights_for(asin)
+  if not $cached_highlights_for.has_key? asin
+    puts "loading highlights for #{asin}"
+    $cached_highlights_for[asin] = kindle.highlights_for(asin)
+  end
+  highlight_list = Marshal.load(Marshal.dump($cached_highlights_for[asin]))
   
   # extract the mechanize client out of the kindle client
   mech = kindle.instance_variable_get(:@mechanize_agent)
   endpoint = "https://kindle.amazon.com/your_highlights_and_notes/#{asin}"
+  highlights_and_notes_page = load_page(kindle, endpoint)
+  allNoteData = parse_highlights_page(highlights_and_notes_page)
 
-  highlights_and_notes_page = mech.get(endpoint)
-  # realistically, we expect each endLocation to appear only once,
-  # because why would you create 2 highlights to the same passage
-  # ending, but you never know, so we'll make a
-  # Hash[endLocation => highlight]
-  endLocation_mapping = highlight_list.inject({}) do |out, highlight_entry|
-    endLocation = highlight_entry['endLocation']
-    if not out.include? endLocation then
-      out[endLocation] = []
+  # probably will never run, but just in case
+  dupecheck = Hash.new(0)
+  highlight_list.map{|entry| dupecheck[entry['endLocation']] += 1}
+  dupecheck.each do |endLocation, count|
+    if count > 1
+      puts("WARNING! #{endLocation} appears more than once!")
     end
-    out[endLocation].push(highlight_entry)
-    out
   end
 
-  npage = Nokogiri::HTML(highlights_and_notes_page.body)
-  npage.css('.highlightRow.yourHighlight').each do |html_entry|
-    # need to match the endLocation => highlight mapping to be a hit
-    highlight = html_entry.css('.highlight').text
-    endLocation = html_entry.css('.hidden.end_location').text.to_i
-
-    if not endLocation_mapping.include? endLocation then
-      puts('WARNING! MISMATCH OF endLocation FOUND!!!')
-      puts("searched for #{endLocation}")
-      puts("among: #{endLocation_mapping.keys}")
-      next
+  note_data = allNoteData[:noteMapping].clone
+  #
+  # it turns out some entries have endLocations that are off by 1. wtf?
+  # so we will loop over different offset values until everything is
+  # matched or we give up
+  [0, 1, -1, 2, -2].each do |try_offset|
+    if try_offset != 0
+      puts "... retrying with offset #{try_offset}"
     end
-
-    note_text = html_entry.css('.noteContent').text
-    if note_text.length == 0 then
-      next
-    end
-
-    endLocation_mapping[endLocation].each do |highlight_entry|
-      if highlight_entry['highlight'] == highlight then
-        # exact match!!!
-        # puts("modifying entry with note!")
-        highlight_entry['note'] = note_text
-        break
+    highlight_list.each do |entry|
+      endLocation = entry['endLocation'] + try_offset
+      note_text = note_data[endLocation]
+      if not note_text.nil?
+        if entry.include? 'note' then
+          puts "CONFLICT!"
+          puts "    #{note_text}"
+          puts " at #{endLocation}"
+        else
+          entry['note'] = note_text
+        end
       end
+      note_data.delete endLocation
+    end
+
+    if note_data.empty?
+      puts "everything checks out!"
+      break
+    else
+      puts "#{note_data.length} notes remaining to match."
     end
   end
 
-  highlight_list
+  note_data.each do |endLocation, note|
+    puts "NOT MATCHED:"
+    puts "    #{note}"
+    puts " at #{endLocation}"
+  end
+
+  return highlight_list
+end
+
+def get_most_recent_book(kindle)
+  endpoint = "https://kindle.amazon.com/your_highlights"
+  # most recent will appear as first result. if you scroll down, more will
+  # load. but we only want page 1
+  most_recent_highlights_page = load_page(kindle, endpoint)
+  parse_highlights_page(most_recent_highlights_page)
+end
+
+def make_output_filename(book_name)
+  "#{book_name}.yml"
+end
+
+def save(basedir, k, asin)
+  puts k.books[asin]
+  ofile = File.join(basedir, make_output_filename(k.books[asin]))
+  File.open(ofile, 'w') {|f| f.write({
+    'title' => k.books[asin],
+    'highlight_list' => get_combined_annotation_list(k, asin),
+  }.to_yaml)}
+  "ok: #{ofile}"
 end
 
 if __FILE__ == $0
